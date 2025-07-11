ceedafaa456414f81b747fd60574cdb61fad9de2
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-11 16:17:13 +0200

    feature: setup translations

---
+++ src/services/translations.tsx
@@ -0,0 +1,79 @@
+import { createContext, useCallback, useContext } from "react";
+import { PluralizeOptionsType, pluralize } from "./pluralize";
+
+export type TranslationsKeyType = string;
+
+export type TranslationsValueType = string;
+
+export type TranslationsType = Record<TranslationsKeyType, TranslationsValueType>;
+
+type TranslationPlaceholderType = string;
+
+type TranslationPlaceholderValueType = string | number;
+
+type TranslationVariableType = Record<TranslationPlaceholderType, TranslationPlaceholderValueType>;
+
+export type TranslationsContextValueType = {
+  translations: TranslationsType;
+  language: string;
+};
+
+const TranslationsContext = createContext<TranslationsContextValueType>({
+  translations: {},
+  language: "en",
+});
+
+type TranslationsContextPropsType = {
+  children: React.ReactNode;
+  value: TranslationsContextValueType;
+};
+
+export function TranslationsContextProvider(props: TranslationsContextPropsType) {
+  return <TranslationsContext.Provider value={props.value}>{props.children}</TranslationsContext.Provider>;
+}
+
+export function useTranslations() {
+  const value = useContext(TranslationsContext);
+
+  if (value === undefined) {
+    throw new Error("useTranslations must be used within the TranslationsContext");
+  }
+
+  const translate = useCallback(
+    (key: TranslationsKeyType, variables?: TranslationVariableType) => {
+      const translation = value.translations[key];
+
+      if (!translation) {
+        // biome-ignore lint: lint/suspicious/noConsole
+        console.warn(`[@bgord/ui] missing translation for key: ${key}`);
+        return key;
+      }
+
+      if (!variables) return translation;
+
+      return Object.entries(variables).reduce((result, [placeholder, value]) => {
+        const regex = new RegExp(`{{${placeholder}}}`, "g");
+        return result.replace(regex, String(value));
+      }, translation);
+    },
+    [value.translations],
+  );
+
+  return translate;
+}
+
+export function useLanguage(): TranslationsContextValueType["language"] {
+  const value = useContext(TranslationsContext);
+
+  if (value === undefined) {
+    throw new Error("useLanguage must be used within the TranslationsContext");
+  }
+
+  return value.language;
+}
+
+export function usePluralize() {
+  const language = useLanguage();
+
+  return (options: Omit<PluralizeOptionsType, "language">) => pluralize({ ...options, language });
+}
