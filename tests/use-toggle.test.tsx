959be475b6924249dbb5e3bd9f8503c47e021606
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-24 10:08:58 +0200

    refactor: improve use toggle tests

--- tests/use-toggle.test.tsx
+++ tests/use-toggle.test.tsx
@@ -57,12 +57,10 @@ describe("useToggle", () => {
     const hook = renderHook(() => useToggle({ name: "test" }));
 
     act(() => hook.result.current.enable());
-
     expect(hook.result.current.on).toBe(true);
     expect(hook.result.current.off).toBe(false);
 
     act(() => hook.result.current.enable());
-
     expect(hook.result.current.on).toBe(true);
   });
 
@@ -70,7 +68,6 @@ describe("useToggle", () => {
     const hook = renderHook(() => useToggle({ name: "test", defaultValue: true }));
 
     act(() => hook.result.current.disable());
-
     expect(hook.result.current.on).toBe(false);
     expect(hook.result.current.off).toBe(true);
 
@@ -78,21 +75,16 @@ describe("useToggle", () => {
     expect(hook.result.current.on).toBe(false);
     expect(hook.result.current.off).toBe(true);
   });
-});
 
-describe("extractUseToggle", () => {
-  test("happy path", () => {
+  test("extractUseToggle", () => {
     const props = { className: "test-class", style: { color: "red" } };
-
     const hook = renderHook(() => useToggle({ name: "test" }));
-
     const { toggle, rest } = extractUseToggle({ ...hook.result.current, ...props });
 
     expect(toggle).toEqual(hook.result.current);
 
     // @ts-expect-error
     expect(rest).toEqual(props);
-
     expect(rest).not.toHaveProperty("on");
     expect(rest).not.toHaveProperty("off");
     expect(rest).not.toHaveProperty("enable");
@@ -100,95 +92,8 @@ describe("extractUseToggle", () => {
     expect(rest).not.toHaveProperty("toggle");
     expect(rest).not.toHaveProperty("props");
   });
-});
 
-describe("useToggle in components", () => {
-  test("toggle", () => {
-    function Testcase() {
-      const message = useToggle({ name: "test" });
-
-      return (
-        <div>
-          <button type="button" onClick={message.toggle} {...message.props.controller}>
-            Toggler
-          </button>
-          <div {...message.props.target} style={{ display: message.on ? "block" : "none" }}>
-            Message
-          </div>
-        </div>
-      );
-    }
-
-    render(<Testcase />);
-
-    const button = screen.getByText("Toggler");
-    const message = screen.queryByText("Message");
-
-    expect(button).toHaveAttribute("aria-expanded", "false");
-    expect(button).toHaveAttribute("aria-controls", "test");
-    expect(message).toHaveAttribute("id", "test");
-    expect(message).not.toBeVisible();
-
-    fireEvent.click(button);
-
-    expect(button).toHaveAttribute("aria-expanded", "true");
-    expect(message).toBeVisible();
-
-    fireEvent.click(button);
-
-    expect(button).toHaveAttribute("aria-expanded", "false");
-    expect(message).not.toBeVisible();
-
-    cleanup();
-  });
-
-  test("enable/disable", () => {
-    function Testcase() {
-      const message = useToggle({ name: "test" });
-
-      return (
-        <div>
-          <button type="button" onClick={message.enable}>
-            Show
-          </button>
-          <button type="button" onClick={message.disable}>
-            Hide
-          </button>
-          <div {...message.props.target} style={{ display: message.on ? "block" : "none" }}>
-            Message
-          </div>
-        </div>
-      );
-    }
-
-    render(<Testcase />);
-
-    const show = screen.getByText("Show");
-    const hide = screen.getByText("Hide");
-    const message = screen.getByText("Message");
-
-    expect(message).not.toBeVisible();
-
-    fireEvent.click(show);
-
-    expect(message).toBeVisible();
-
-    fireEvent.click(show);
-
-    expect(message).toBeVisible();
-
-    fireEvent.click(hide);
-
-    expect(message).not.toBeVisible();
-
-    fireEvent.click(hide);
-
-    expect(message).not.toBeVisible();
-
-    cleanup();
-  });
-
-  test("default value - true", () => {
+  test("integration - default value - true", () => {
     function Testcase() {
       const message = useToggle({ defaultValue: true, name: "test" });
 
@@ -216,6 +121,85 @@ describe("useToggle in components", () => {
     fireEvent.click(button);
     expect(message).not.toBeVisible();
     expect(button).toHaveAttribute("aria-expanded", "false");
+
+    cleanup();
+  });
+
+  test("integration - toggle", () => {
+    function Testcase() {
+      const message = useToggle({ name: "test" });
+
+      return (
+        <div>
+          <button type="button" onClick={message.toggle} {...message.props.controller}>
+            Toggler
+          </button>
+          <div {...message.props.target} style={{ display: message.on ? "block" : "none" }}>
+            Message
+          </div>
+        </div>
+      );
+    }
+
+    render(<Testcase />);
+
+    const button = screen.getByText("Toggler");
+    const message = screen.queryByText("Message");
+
+    expect(button).toHaveAttribute("aria-expanded", "false");
+    expect(button).toHaveAttribute("aria-controls", "test");
+    expect(message).toHaveAttribute("id", "test");
+    expect(message).not.toBeVisible();
+
+    fireEvent.click(button);
+    expect(button).toHaveAttribute("aria-expanded", "true");
+    expect(message).toBeVisible();
+
+    fireEvent.click(button);
+    expect(button).toHaveAttribute("aria-expanded", "false");
+    expect(message).not.toBeVisible();
+
+    cleanup();
+  });
+
+  test("integration - enable/disable", () => {
+    function Testcase() {
+      const message = useToggle({ name: "test" });
+
+      return (
+        <div>
+          <button type="button" onClick={message.enable}>
+            Show
+          </button>
+          <button type="button" onClick={message.disable}>
+            Hide
+          </button>
+          <div {...message.props.target} style={{ display: message.on ? "block" : "none" }}>
+            Message
+          </div>
+        </div>
+      );
+    }
+
+    render(<Testcase />);
+
+    const show = screen.getByText("Show");
+    const hide = screen.getByText("Hide");
+    const message = screen.getByText("Message");
+
+    expect(message).not.toBeVisible();
+    fireEvent.click(show);
+
+    expect(message).toBeVisible();
+    fireEvent.click(show);
+
+    expect(message).toBeVisible();
+    fireEvent.click(hide);
+
+    expect(message).not.toBeVisible();
+    fireEvent.click(hide);
+
+    expect(message).not.toBeVisible();
 
     cleanup();
   });
