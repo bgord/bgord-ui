03254ec178c036bf832ce1137cd3d9346de853fd
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-24 09:45:05 +0200

    step: add Cookies.set

--- src/services/cookies.ts
+++ src/services/cookies.ts
@@ -1,6 +1,11 @@
-//TODO: tests
+import JsCookie from "js-cookie";
+
 export class Cookies {
   static extractFrom(request: Request): string {
     return request.headers.get("cookie") ?? "";
+  }
+
+  static set(name: string, value: string) {
+    JsCookie.set(name, value);
   }
 }
--- tests/cookies.ts
+++ tests/cookies.ts
@@ -1,12 +1,19 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, spyOn, test } from "bun:test";
+import JsCookie from "js-cookie";
 import { Cookies } from "../src/services/cookies";
 
 describe("Cookies", () => {
   test("extracts cookie header from Response", () => {
-    const request = new Request("exapmle.com", {
-      headers: { cookie: "example" },
-    });
+    const request = new Request("exapmle.com", { headers: { cookie: "example" } });
 
     expect(Cookies.extractFrom(request)).toEqual("example");
+  });
+
+  test("set", () => {
+    const jsCookieSetSpy = spyOn(JsCookie, "set");
+
+    Cookies.set("name", "test");
+
+    expect(jsCookieSetSpy).toHaveBeenCalled();
   });
 });
