a879a98239003b4ded6e30fe4e3faf887fb7bb53
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-24 11:14:14 +0200

    refactor: improve form

--- src/services/form.ts
+++ src/services/form.ts
@@ -1,33 +1,38 @@
 import type React from "react";
 
-type PatternConfigType = {
+type PatternTextConfigType = {
   min?: number;
   max?: number;
   required?: React.JSX.IntrinsicElements["input"]["required"];
 };
 
+type PatternExactConfigType = {
+  text: string;
+  required?: React.JSX.IntrinsicElements["input"]["required"];
+};
+
 export class Form {
-  static inputPattern(config: PatternConfigType): React.ComponentPropsWithoutRef<"input"> {
+  static input(config: PatternTextConfigType): React.ComponentPropsWithoutRef<"input"> {
     const required = config.required ?? true;
 
     if (config.min && !config.max) return { pattern: `.{${config.min}}`, required };
-
     if (config.min && config.max) return { pattern: `.{${config.min},${config.max}}`, required };
-
     if (!config.min && config.max) return { pattern: `.{,${config.max}}`, required };
-
     return { pattern: undefined, required };
   }
 
-  static textareaPattern(config: PatternConfigType): React.ComponentPropsWithoutRef<"textarea"> {
+  static textarea(config: PatternTextConfigType): React.ComponentPropsWithoutRef<"textarea"> {
     const required = config.required ?? true;
 
     if (config.min && !config.max) return { minLength: config.min, required };
-
     if (config.min && config.max) return { minLength: config.min, maxLength: config.max, required };
-
     if (!config.min && config.max) return { maxLength: config.max, required };
-
     return { required };
+  }
+
+  static exact(config: PatternExactConfigType): React.ComponentPropsWithoutRef<"input"> {
+    const required = config.required ?? true;
+
+    return { pattern: config.text, required };
   }
 }
--- tests/form.test.ts
+++ tests/form.test.ts
@@ -2,114 +2,83 @@ import { describe, expect, test } from "bun:test";
 import { Form } from "../src/services/form";
 
 describe("Form", () => {
-  describe("inputPattern", () => {
-    test("default value", () => {
-      expect(Form.inputPattern({})).toEqual({ pattern: undefined, required: true });
-    });
+  test("input -default value", () => {
+    expect(Form.input({})).toEqual({ pattern: undefined, required: true });
+  });
 
-    test("default value - non required", () => {
-      expect(Form.inputPattern({ required: false })).toEqual({
-        pattern: undefined,
-        required: false,
-      });
-    });
+  test("input - default value - non required", () => {
+    expect(Form.input({ required: false })).toEqual({ pattern: undefined, required: false });
+  });
 
-    test("min - required", () => {
-      expect(Form.inputPattern({ min: 1, required: true })).toEqual({
-        pattern: ".{1}",
-        required: true,
-      });
-    });
+  test("input - min - required", () => {
+    expect(Form.input({ min: 1, required: true })).toEqual({ pattern: ".{1}", required: true });
+  });
 
-    test("min - non required", () => {
-      expect(Form.inputPattern({ min: 1, required: false })).toEqual({
-        pattern: ".{1}",
-        required: false,
-      });
-    });
+  test("input - min - non required", () => {
+    expect(Form.input({ min: 1, required: false })).toEqual({ pattern: ".{1}", required: false });
+  });
 
-    test("max - required", () => {
-      expect(Form.inputPattern({ max: 2, required: true })).toEqual({
-        pattern: ".{,2}",
-        required: true,
-      });
-    });
+  test("input - max - required", () => {
+    expect(Form.input({ max: 2, required: true })).toEqual({ pattern: ".{,2}", required: true });
+  });
 
-    test("max - non required", () => {
-      expect(Form.inputPattern({ max: 2, required: false })).toEqual({
-        pattern: ".{,2}",
-        required: false,
-      });
-    });
+  test("input - max - non required", () => {
+    expect(Form.input({ max: 2, required: false })).toEqual({ pattern: ".{,2}", required: false });
+  });
 
-    test("min + max - required", () => {
-      expect(Form.inputPattern({ min: 1, max: 2, required: true })).toEqual({
-        pattern: ".{1,2}",
-        required: true,
-      });
-    });
+  test("input - min + max - required", () => {
+    expect(Form.input({ min: 1, max: 2, required: true })).toEqual({ pattern: ".{1,2}", required: true });
+  });
 
-    test("min + max - non required", () => {
-      expect(Form.inputPattern({ min: 1, max: 2, required: false })).toEqual({
-        pattern: ".{1,2}",
-        required: false,
-      });
+  test("input - min + max - non required", () => {
+    expect(Form.input({ min: 1, max: 2, required: false })).toEqual({ pattern: ".{1,2}", required: false });
+  });
+
+  test("textarea - default value", () => {
+    expect(Form.textarea({})).toEqual({ required: true });
+  });
+
+  test("textarea - default value - non required", () => {
+    expect(Form.textarea({ required: false })).toEqual({ required: false });
+  });
+
+  test("textarea - min - required", () => {
+    expect(Form.textarea({ min: 1, required: true })).toEqual({ minLength: 1, required: true });
+  });
+
+  test("textarea - min - non required", () => {
+    expect(Form.textarea({ min: 1, required: false })).toEqual({ minLength: 1, required: false });
+  });
+
+  test("textarea - max - required", () => {
+    expect(Form.textarea({ max: 2, required: true })).toEqual({ maxLength: 2, required: true });
+  });
+
+  test("textarea - max - non required", () => {
+    expect(Form.textarea({ max: 2, required: false })).toEqual({ maxLength: 2, required: false });
+  });
+
+  test("textarea - min + max - required", () => {
+    expect(Form.textarea({ min: 1, max: 2, required: true })).toEqual({
+      minLength: 1,
+      maxLength: 2,
+      required: true,
     });
   });
 
-  describe("textareaPattern", () => {
-    test("default value", () => {
-      expect(Form.textareaPattern({})).toEqual({ required: true });
+  test("textarea - min + max - non required", () => {
+    expect(Form.textarea({ min: 1, max: 2, required: false })).toEqual({
+      minLength: 1,
+      maxLength: 2,
+      required: false,
     });
+  });
 
-    test("default value - non required", () => {
-      expect(Form.textareaPattern({ required: false })).toEqual({
-        required: false,
-      });
-    });
+  test("exact - required", () => {
+    expect(Form.exact({ text: "delete" })).toEqual({ pattern: "delete", required: true });
+  });
 
-    test("min - required", () => {
-      expect(Form.textareaPattern({ min: 1, required: true })).toEqual({
-        minLength: 1,
-        required: true,
-      });
-    });
-
-    test("min - non required", () => {
-      expect(Form.textareaPattern({ min: 1, required: false })).toEqual({
-        minLength: 1,
-        required: false,
-      });
-    });
-
-    test("max - required", () => {
-      expect(Form.textareaPattern({ max: 2, required: true })).toEqual({
-        maxLength: 2,
-        required: true,
-      });
-    });
-
-    test("max - non required", () => {
-      expect(Form.textareaPattern({ max: 2, required: false })).toEqual({
-        maxLength: 2,
-        required: false,
-      });
-    });
-
-    test("min + max - required", () => {
-      expect(Form.textareaPattern({ min: 1, max: 2, required: true })).toEqual({
-        minLength: 1,
-        maxLength: 2,
-        required: true,
-      });
-    });
-
-    test("min + max - non required", () => {
-      expect(Form.textareaPattern({ min: 1, max: 2, required: false })).toEqual({
-        minLength: 1,
-        maxLength: 2,
-        required: false,
-      });
-    });
+  test("exact - non required", () => {
+    expect(Form.exact({ text: "delete", required: false })).toEqual({ pattern: "delete", required: false });
   });
 });
