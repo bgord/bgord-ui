5dc58b1812438b1e35d279cc96a634b7a21555c7
Bartosz Gordon <gordonbartosz@gmail.com>
2025-10-25 19:26:51 +0200

    refactor: remove autocomplete types

--- src/services/autocomplete.ts
+++ src/services/autocomplete.ts
@@ -1,10 +1,4 @@
-type AutocompleteType = {
-  email: React.JSX.IntrinsicElements["input"];
-  password: { new: React.JSX.IntrinsicElements["input"]; current: React.JSX.IntrinsicElements["input"] };
-  off: React.JSX.IntrinsicElements["input"];
-};
-
-export const Autocomplete: AutocompleteType = {
+export const Autocomplete = {
   email: { inputMode: "email", autoComplete: "email", autoCapitalize: "none", spellCheck: "false" },
   password: { new: { autoComplete: "new-password" }, current: { autoComplete: "current-password" } },
   off: { autoComplete: "off", spellCheck: false },
