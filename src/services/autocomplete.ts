6d28bd07d43166f21dfca8acff66e74395542174
Bartosz Gordon <gordonbartosz@gmail.com>
2026-04-28 12:51:34 +0200

    fix: make autocomplete const

--- src/services/autocomplete.ts
+++ src/services/autocomplete.ts
@@ -2,4 +2,4 @@ export const Autocomplete = {
   email: { inputMode: "email", autoComplete: "email", autoCapitalize: "none", spellCheck: "false" },
   password: { new: { autoComplete: "new-password" }, current: { autoComplete: "current-password" } },
   off: { autoComplete: "off", spellCheck: false },
-};
+} as const;
